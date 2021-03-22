e7d8597ec6a9fdb6337c64e6e67885af70c834a0
gferreira97 <gabriiel.fmarinho@gmail.com>
2021-03-22 15:37:29 -0300

    vesionation with media types

--- src/main/kotlin/com/poc/with/spring/controller/product/ProductController.kt
+++ src/main/kotlin/com/poc/with/spring/controller/product/ProductController.kt
@@ -1,5 +1,6 @@
 package com.poc.with.spring.controller.product
 
+import com.poc.with.spring.controller.product.MediaTypes.Companion.MEDIA_TYPE_V1
 import com.poc.with.spring.controller.product.request.CreateProductRequest
 import com.poc.with.spring.controller.product.request.UpdateProductRequest
 import com.poc.with.spring.controller.product.response.ProductResponse
@@ -29,52 +30,52 @@ class ProductController(
         val log = loggerFor(ProductController.javaClass)
     }
 
-    @PostMapping
+    @PostMapping(produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun create(@RequestBody createProductRequest: CreateProductRequest): ResponseEntity<Unit> {
         log.info("receive request for create product, productRequest=${createProductRequest}")
         saveProductService.create(createProductRequest.toProductCreateDTO())
         return ResponseEntity(Unit, HttpStatus.CREATED)
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}", produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun getById(@PathVariable id: Long): ResponseEntity<ProductResponse> {
         log.info("receive request, find product by id=${id}")
         val productDTO = findProductService.findById(id)
         return ResponseEntity.ok(productDTO.toProductResponse())
     }
 
-    @GetMapping
+    @GetMapping(produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun getByDescription(@RequestParam description: String ): ResponseEntity<List<ProductResponse>> {
         log.info("receive request, find product by description=${description}")
         val productsDTO = findProductService.findByDescription(description);
         return ResponseEntity.ok( productsDTO.map { productDTO -> productDTO.toProductResponse() })
     }
 
-    @GetMapping("/all")
+    @GetMapping("/all", produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun getAllPageable(@RequestParam page: Int, @RequestParam quantityPerPage: Int ): ResponseEntity<Page<ProductResponse>> {
         log.info("receive request, find all product pageable")
         return ResponseEntity.ok(findProductService.findByAll(page, quantityPerPage)
             .map { productDTO -> productDTO.toProductResponse() })
     }
 
-    @PatchMapping
+    @PatchMapping(produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun update(@RequestBody updateProductRequest: UpdateProductRequest): ResponseEntity<Any> {
         log.info("receive request, update  product with name=${updateProductRequest.name}")
         updateProductService.update(updateProductRequest.toProductUpdateDTO())
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().build()
     }
 
-    @PutMapping("/inactivate")
+    @PutMapping("/inactivate", produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun inactive(@RequestParam name: String): ResponseEntity<Any> {
         log.info("receive request, inactivate product with name=${name}")
         updateProductService.inactivate(name)
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().build()
     }
 
-    @DeleteMapping
+    @DeleteMapping(produces = [MEDIA_TYPE_V1], consumes = [MEDIA_TYPE_V1])
     fun delete(@RequestParam name: String): ResponseEntity<Any> {
         log.info("receive request, delete product with name=${name}")
         deleteProductService.delete(name)
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().build()
     }
 }
--- src/test/kotlin/com/poc/with/spring/PocWithSpringApplicationTests.kt
+++ src/test/kotlin/com/poc/with/spring/PocWithSpringApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class PocWithSpringApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
-
 }
